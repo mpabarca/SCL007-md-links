818cd6be0bf5dd3d53c205c4cf116200826e5ed4
mpabarca <mpabarca@uc.cl>
2019-02-27 19:36:21 -0300

    se hace funcion validateLink

--- index.js
+++ index.js
@@ -5,10 +5,26 @@ const path = require('path');
 const markdownLinkExtractor = require('markdown-link-extractor');
 const validUrl = require('valid-url');
 
-//COMPORTAMIENTO POR DEFECTO
-
 var read = util.promisify(fs.readFile);
 let name = './lorem.md'
+
+//funcion que valida los links
+function validateLinks(link){
+  return new Promise ((resolve,reject) => {
+    fetch(link)
+    .then(res =>{
+      if (res.status===200){
+        return true;
+      }else {
+        return false;
+      }
+    })
+    .catch(error => {
+      console.log('error', error);
+    })
+  })
+}
+//COMPORTAMIENTO POR DEFECTO
 read(name)
   .then(data => {
     let str=data.toString()
@@ -30,4 +46,16 @@ read(name)
   .catch(error => {
     console.log('error', error);
   })
-
+// --VALIDATE
+read(name)
+  .then(data => {
+    let str=data.toString()
+    return(str);
+  })
+  .then(res => {
+    let links = markdownLinkExtractor(res);
+    
+  })
+  .catch(error => {
+    console.log('error', error);
+  })
