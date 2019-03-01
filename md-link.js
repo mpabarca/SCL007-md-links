5bbd498d1ba3c9da744f45ecd8e8f8a877f5f7bb
mpabarca <mpabarca@uc.cl>
2019-03-01 16:04:12 -0300

    se crea archivo con modulo md-link.js

---
+++ md-link.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+const fetch = require('node-fetch');
+const util = require('util');
+const path = require('path');
+const markdownLinkExtractor = require('markdown-link-extractor');
+
+//creacion de modulo
+module.exports = {
+
+mdLinks: async (pathRuta) => {
+    let ruta = path.resolve(documentUser);
+    var read = util.promisify(fs.readFile);
+    let readMarkdown = (await read(ruta)).toString();
+    let links = markdownLinkExtractor(readMarkdown);
+    let arrayLink= [];
+    links.forEach(function(link){
+      let arrayFetch = fetch(link)
+        .then((res) => {
+          let objectLink = {
+            href: link,
+            text: 'texto',
+            file: ruta,
+            status:res.status
+          }
+          return objectLink;
+        })
+        .catch((error) =>{
+          let fail = {
+            hrefLink: link,
+            statusLink: 'error'
+          }
+        })
+      
+      arrayLink.push(arrayFetch);
+    })
+    Promise.all(arrayLink)
+      .then((values)=>{
+          console.log(values)
+      })
+      .catch(console.error)
+  },
+validate: (link) =>{
+    
+}
+
+
+} 
